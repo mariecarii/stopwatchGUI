6f87ee2df8bc8ecb2f000d876487c4f0920b6503
Marie Cari <mcari@nyc.yearup.org>
2022-12-18 22:26:27 -0500

    basic JFrame layout created

--- Stopwatch.java
+++ Stopwatch.java
@@ -1,2 +1,44 @@
-public class Stopwatch {
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Stopwatch implements ActionListener {
+    //declare variables
+
+    JFrame frame = new JFrame();
+    JButton startButton = new JButton("Start");
+    JButton resetButton = new JButton("Restart");
+    JLabel timeLabel = new JLabel();
+    int elapsedTime = 0;
+    int seconds = 0;
+    int minutes = 0;
+    int hours = 0;
+    boolean started = false;
+    //format
+    String seconds_string = String.format("%02d", seconds);
+    String minutes_string = String.format("%02d", minutes);
+    String hours_string = String.format("%02d", hours);
+
+    //constructor
+    Stopwatch() {
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300, 300);
+        frame.setLayout(null);
+        frame.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    void start() {
+
+    }
+    void stop() {
+
+    }
+    void reset() {
+
+    }
 }
